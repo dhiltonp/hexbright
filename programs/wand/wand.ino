36344c30989bd1286a15e56f1be64a69c7467e2a
David P Hilton <david.hilton.p@gmail.com>
2012-12-20 12:55:09 -0700

    modified wand to only output light at the end of a swing

--- programs/wand/wand.ino
+++ programs/wand/wand.ino
@@ -23,11 +23,28 @@ void loop() {
   }
   
   if(mode==WAND_MODE) {
+    static double last_dp = 1;
+    static int highest_level = 1;
+    double dp = hb.get_dp();
+    
+
     // sample rate is 120hz, so every 8 ms or so we get a new reading.
 //    hb.print_accelerometer(); // and dot product, cos_alpha
     // we will be 120/10th of the way to the new brightness the next time we come through.
     // dp is in Gs, 1 = gravity
-    hb.set_light(CURRENT_LEVEL, abs(hb.get_dp()-1)*500, 120); 
+    
+//    hb.set_light(CURRENT_LEVEL, abs(hb.get_dp()-1)*500, 120); 
+    if(dp-last_dp>1.1) {
+      highest_level = (dp-last_dp)*350;
+      highest_level = highest_level>1000 ? 1000 : highest_level;
+    } else if (highest_level) {
+      hb.set_light(highest_level, 0, 300); 
+      highest_level = 0;
+    }
+    last_dp = dp;
+    
+    
+    
   } else if (mode==OFF_MODE) {
     hb.shutdown(); 
     if(hb.get_charge_state()==CHARGED) {
