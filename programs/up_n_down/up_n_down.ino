1458084e0228c56389c807d7fd796aa17f1345db
Whitney Battestilli <wbattestilli@itsnc.net>
2013-05-01 01:36:13 -0400

    update up_n_down to use new library click counter + some other optimizations

--- programs/up_n_down/up_n_down.ino
+++ programs/up_n_down/up_n_down.ino
@@ -42,21 +42,21 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
  // Defaults
 static const int glow_mode_time = 3000;
-static const int short_click = 350; // maximum time for a "short" click
+static const int click = 350; // maximum time for a "short" click
 static const int nightlight_timeout = 10000; // timeout before nightlight powers down after any movement
 static const unsigned char nightlight_sensitivity = 20; // measured in 100's of a G.
 
 // State
 static unsigned long treg1=0; 
 
-static word bitreg=0;
+static int bitreg=0;
 const unsigned BLOCK_TURNING_OFF=0;
 const unsigned GLOW_MODE=1;
 const unsigned GLOW_MODE_SET=2;
+const unsigned QUICKSTROBE=3;
 
 static char mode = MODE_OFF;
 static char new_mode = MODE_OFF;  
-static char click_count = 0;
 
 static word nightlight_brightness;
 
@@ -67,14 +67,14 @@ static byte locked;
 hexbright hb;
 
 int adjustLED() {
-  if(hb.button_pressed() && hb.button_pressed_time()>short_click) {
+  if(hb.button_pressed() && hb.button_pressed_time()>click) {
     double d = hb.difference_from_down();
     if(d>=0 && d<=1.0) {
       int i = (int)(d * 2000.0);
       i = i>1000 ? 1000 : i;
       i = i<=0 ? 1 : i;
       hb.set_light(CURRENT_LEVEL, i, 0);
-      bitreg |= (1<<BLOCK_TURNING_OFF);
+      BIT_SET(bitreg,BLOCK_TURNING_OFF);
       return i;
     }
   }
@@ -105,6 +105,8 @@ void setup() {
   Serial.print("Nightlight Brightness: "); Serial.println(nightlight_brightness);  
   
   Serial.println("Powered up!");
+
+  hb.config_click_count(click);
 } 
 
 void loop() {
@@ -112,65 +114,59 @@ void loop() {
   int i;
 
   hb.update();
-
-  // Charging
+  
 #ifdef PRINTING_NUMBER:
   if(!hb.printing_number()) 
 #endif
   {
-    if(!(bitreg & (1<<GLOW_MODE))) {
-      hb.print_charge(GLED);
-    }
+    // Charging
+    hb.print_charge(GLED);
 
+    // Low battery
     if(mode != MODE_OFF && hb.low_voltage_state())
       if(hb.get_led_state(RLED)==LED_OFF) 
 	hb.set_led(RLED,50,1000);
   }
 
- // Check for mode and do in-mode activities
+  new_mode = hb.click_count();
+  if(new_mode>=MODE_OFF) {
+    Serial.print("Clicks recieved: "); Serial.println((int)new_mode);
+    if(mode!=MODE_OFF) {
+      Serial.println("Forcing MODE_OFF");
+      new_mode=MODE_OFF;
+    } 
+  }
+
+  // Check for mode and do in-mode activities
   switch(mode) {
   case MODE_OFF:
     // glow mode
-    if(bitreg & (1<<GLOW_MODE))
+    if(BIT_CHECK(bitreg,GLOW_MODE))
       hb.set_led(GLED, 100);
-    
-    // click counting
-    if(hb.button_just_released()) {
-      bitreg &= ~(1<<GLOW_MODE_SET);
-      if(hb.button_pressed_time() <= short_click) {
-	click_count++;
-      } 
-    } 
-    
-    // request mode change
-    if(click_count && hb.button_released_time() >= short_click) {
-      // we're done clicking
-      if(click_count > MAX_MODE) {
-	click_count = 0;
-      }
-      new_mode=click_count;
-      click_count=0;
-    }
     
     // holding the button
     if(hb.button_pressed() && !locked) {
-      if(!(bitreg & (1<<GLOW_MODE_SET))) {
 	double d = hb.difference_from_down();
-	if(hb.button_pressed_time() >= glow_mode_time && d <= 0.1) {
-	  bitreg ^= (1<<GLOW_MODE);
-	  bitreg |= (1<<GLOW_MODE_SET);
-	  if(bitreg & (1<<GLOW_MODE))
+	if(BIT_CHECK(bitreg,QUICKSTROBE) || (hb.button_pressed_time() > click && d > 0.10 )) {
+	  BIT_SET(bitreg,QUICKSTROBE);
+	  if(treg1+blink_freq_map[0] < time) { 
+	    treg1 = time; 
+	    hb.set_light(MAX_LEVEL, 0, 20); 
+	  }
+	}
+	if(hb.button_pressed_time() >= glow_mode_time && d <= 0.1 && 
+	   !BIT_CHECK(bitreg,GLOW_MODE_SET) && !BIT_CHECK(bitreg,QUICKSTROBE) ) {
+	  BIT_TOGGLE(bitreg,GLOW_MODE);
+	  BIT_SET(bitreg,GLOW_MODE_SET);
+	  if(BIT_CHECK(bitreg,GLOW_MODE))
 	    Serial.println("Glow mode");
 	  else
 	    Serial.println("Glow mode off");	
-	} else if(hb.button_pressed_time() > short_click && d > 0.10) {
-	  // quickstrobe
-	  if(treg1+blink_freq_map[0] < time) { 
-	      treg1 = time; 
-	      hb.set_light(MAX_LEVEL, 0, 20); 
-	    }
-	}
-      }
+	} 
+    }
+    if(hb.button_just_released()) {
+      BIT_CLEAR(bitreg,GLOW_MODE_SET);
+      BIT_CLEAR(bitreg,QUICKSTROBE);
     }
     break;
   case MODE_LEVEL:
@@ -198,7 +194,7 @@ void loop() {
     break; }
   case MODE_BLINK:
     if(hb.button_pressed()) {
-      if( hb.button_pressed_time()>short_click) {
+      if( hb.button_pressed_time()> click) {
 	double d = hb.difference_from_down();
 	if(d>=0 && d<=0.99) {
 	  if(d>=0.25) {
@@ -208,7 +204,7 @@ void loop() {
 	    blink_frequency = blink_freq_map[1] + (word)((blink_freq_map[2] - blink_freq_map[1]) * 4 * (0.25-d));
 	  }
 	  Serial.print("Blink Freq: "); Serial.println(blink_frequency);
-	  bitreg |= (1<<BLOCK_TURNING_OFF);
+	  BIT_SET(bitreg,BLOCK_TURNING_OFF);
 	}
       }
     }
@@ -224,75 +220,28 @@ void loop() {
       static char symbols_remaining = 0;
       static byte pattern = 0;
       const char message[] = "SOS";
-      const int millisPerBeat = 150;
-
-      // High byte = length
-      // Low byte  = morse code, LSB first, 0=dot 1=dash
-      const word morse[] = {
-	0x0202, // A .-
-	0x0401, // B -...
-	0x0405, // C -.-.
-	0x0301, // D -..
-	0x0100, // E .
-	0x0404, // F ..-.
-	0x0303, // G --.
-	0x0400, // H ....
-	0x0200, // I ..
-	0x040E, // J .---
-	0x0305, // K -.-
-	0x0402, // L .-..
-	0x0203, // M --
-	0x0201, // N -.
-	0x0307, // O ---
-	0x0406, // P .--.
-	0x040B, // Q --.-
-	0x0302, // R .-.
-	0x0300, // S ...
-	0x0101, // T -
-	0x0304, // U ..-
-	0x0408, // V ...-
-	0x0306, // W .--
-	0x0409, // X -..-
-	0x040D, // Y -.--
-	0x0403, // Z --..
-	0x051F, // 0 -----
-	0x051E, // 1 .----
-	0x051C, // 2 ..---
-	0x0518, // 3 ...--
-	0x0510, // 4 ....-
-	0x0500, // 5 .....
-	0x0501, // 6 -....
-	0x0503, // 7 --...
-	0x0507, // 8 ---..
-	0x050F, // 9 ----.
+      const word morse[] = {	0x0300, // S ...
+				0x0307, // O ---
+				0x0300, // S ...
       };
+      const int millisPerBeat = 150;
       
       if(current_character>=sizeof(message))
       { // we've hit the end of message, turn off.
-        mode = MODE_OFF;
+        //mode = MODE_OFF;
         // reset the current_character, so if we're connected to USB, next printing will still work.
         current_character = 0;
         // return now to skip the following code.
-        return;
+        break;
       }
             
       if(symbols_remaining <= 0) // we're done printing our last character, get the next!
       {
-        char ch = message[current_character];
-        // Remap ASCII to the morse table
-        if      (ch >= 'A' && ch <= 'Z') ch -= 'A';
-        else if (ch >= 'a' && ch <= 'z') ch -= 'a';
-        else if (ch >= '0' && ch <= '9') ch -= '0' - 26;
-        else ch = -1; // character not in table
-      
-        if(ch>=0)
-        {
-          // Extract the symbols and length
-          pattern = morse[ch] & 0x00FF;
-          symbols_remaining = morse[ch] >> 8;
-          // we count space (between dots/dashes) as a symbol to be printed;
-          symbols_remaining *= 2;
-        }
+	// Extract the symbols and length
+	pattern = morse[current_character] & 0x00FF;
+	symbols_remaining = morse[current_character] >> 8;
+	// we count space (between dots/dashes) as a symbol to be printed;
+	symbols_remaining *= 2;
         current_character++;
       }
       
@@ -325,27 +274,20 @@ void loop() {
     break;
   }  
 
-  // we turn off on button release unless a submode change was made and we're blocked
-  if(mode!=MODE_OFF && hb.button_just_released()) {
-    if(bitreg & (1<<BLOCK_TURNING_OFF))
-      bitreg &= ~(1<<BLOCK_TURNING_OFF);
-    else
-      new_mode=MODE_OFF;
-  }
-
   // if we're locked, we prevent all other modes except MODE_LOCKED
   if(locked && new_mode!=MODE_LOCKED)
     new_mode=MODE_OFF;
 
   // Do the actual mode change
-  if(new_mode!=mode) {
+  if(new_mode>=MODE_OFF && new_mode!=mode) {
     double d;
     // we're changing mode
+    Serial.print("New Mode: "); Serial.print((int)new_mode); Serial.print(" Mode: "); Serial.println((int)mode);
     switch(new_mode) {
     case MODE_OFF:
       Serial.println("Mode = off");
       hb.set_light(CURRENT_LEVEL, 0, NOW);
-      if(!(bitreg & (1<<GLOW_MODE)))
+      if(!BIT_CHECK(bitreg,GLOW_MODE))
 	hb.set_light(CURRENT_LEVEL, OFF_LEVEL, NOW);
       break;
     case MODE_LEVEL:
@@ -388,5 +330,5 @@ void loop() {
     }
     mode=new_mode;
   }
- }
+}
   
