53318a0e7b19673faa460371396c955f9466736d
Petr Tesarik <petr@tesarici.cz>
2013-07-16 22:38:38 +0200

    bootloader: use standard SIGNATURE_x macros
    These macros are correctly defined by including <avr/io.h>, so there
    is no need to repeat them in our code.

--- hardware/hexbright/bootloaders/atmega/ATmegaBOOT_168.c
+++ hardware/hexbright/bootloaders/atmega/ATmegaBOOT_168.c
@@ -164,76 +164,6 @@
 #define MONITOR 1
 #endif
 
-/* define various device id's */
-/* manufacturer byte is always the same */
-#define SIG1	0x1E	// Yep, Atmel is the only manufacturer of AVR micros.  Single source :(
-
-#if defined __AVR_ATmega1280__
-#define SIG2	0x97
-#define SIG3	0x03
-
-#elif defined __AVR_ATmega1281__
-#define SIG2	0x97
-#define SIG3	0x04
-
-#elif defined __AVR_ATmega128__
-#define SIG2	0x97
-#define SIG3	0x02
-
-#elif defined __AVR_ATmega64__
-#define SIG2	0x96
-#define SIG3	0x02
-
-#elif defined __AVR_ATmega32__
-#define SIG2	0x95
-#define SIG3	0x02
-
-#elif defined __AVR_ATmega16__
-#define SIG2	0x94
-#define SIG3	0x03
-
-#elif defined __AVR_ATmega8__
-#define SIG2	0x93
-#define SIG3	0x07
-
-#elif defined __AVR_ATmega88__
-#define SIG2	0x93
-#define SIG3	0x0a
-
-#elif defined __AVR_ATmega168__
-#define SIG2	0x94
-#define SIG3	0x06
-
-#elif defined __AVR_ATmega168P__
-#define SIG2	0x94
-#define SIG3	0x0b
-
-#elif defined __AVR_ATmega328P__
-#define SIG2	0x95
-#define SIG3	0x0F
-
-#elif defined __AVR_ATmega162__
-#define SIG2	0x94
-#define SIG3	0x04
-
-#elif defined __AVR_ATmega163__
-#define SIG2	0x94
-#define SIG3	0x02
-
-#elif defined __AVR_ATmega169__
-#define SIG2	0x94
-#define SIG3	0x05
-
-#elif defined __AVR_ATmega8515__
-#define SIG2	0x93
-#define SIG3	0x06
-
-#elif defined __AVR_ATmega8535__
-#define SIG2	0x93
-#define SIG3	0x08
-
-#endif
-
 // Some models have an SPM Control Register,
 // some have an SPM Control and Status Register
 #if defined SPMCSR
@@ -672,11 +602,11 @@ int noreturn main(void)
 			ch = getch();
 			getch();
 			if (ch == 0) {
-				byte_response(SIG1);
+				byte_response(SIGNATURE_0);
 			} else if (ch == 1) {
-				byte_response(SIG2); 
+				byte_response(SIGNATURE_1);
 			} else {
-				byte_response(SIG3);
+				byte_response(SIGNATURE_2);
 			} 
 		} else {
 			getNch(3);
@@ -911,7 +841,7 @@ int noreturn main(void)
 		if (getch() == Sync_CRC_EOP) {
 			static prog_char sig[] = {
 				Resp_STK_INSYNC,
-				SIG1, SIG2, SIG3,
+				SIGNATURE_0, SIGNATURE_1, SIGNATURE_2,
 				Resp_STK_OK, 0};
 			putstr(sig);
 		} else
