123241e8276c5e1941e40ba1f3f39d67531755d0
Petr Tesarik <petr@tesarici.cz>
2013-07-16 21:27:57 +0200

    bootloader: use standard SPM_PAGESIZE macro
    SPM_PAGESIZE is defined correctly in <avr/io.h> for all relevant MCUs.
    However, it is the page size in bytes, not in words, so it must be
    divided by 2.
    
    This commit has no effect on the generated code.

--- hardware/hexbright/bootloaders/atmega/ATmegaBOOT_168.c
+++ hardware/hexbright/bootloaders/atmega/ATmegaBOOT_168.c
@@ -171,82 +171,66 @@
 #if defined __AVR_ATmega1280__
 #define SIG2	0x97
 #define SIG3	0x03
-#define PAGE_SIZE	0x80U	//128 words
 
 #elif defined __AVR_ATmega1281__
 #define SIG2	0x97
 #define SIG3	0x04
-#define PAGE_SIZE	0x80U	//128 words
 
 #elif defined __AVR_ATmega128__
 #define SIG2	0x97
 #define SIG3	0x02
-#define PAGE_SIZE	0x80U	//128 words
 
 #elif defined __AVR_ATmega64__
 #define SIG2	0x96
 #define SIG3	0x02
-#define PAGE_SIZE	0x80U	//128 words
 
 #elif defined __AVR_ATmega32__
 #define SIG2	0x95
 #define SIG3	0x02
-#define PAGE_SIZE	0x40U	//64 words
 
 #elif defined __AVR_ATmega16__
 #define SIG2	0x94
 #define SIG3	0x03
-#define PAGE_SIZE	0x40U	//64 words
 
 #elif defined __AVR_ATmega8__
 #define SIG2	0x93
 #define SIG3	0x07
-#define PAGE_SIZE	0x20U	//32 words
 
 #elif defined __AVR_ATmega88__
 #define SIG2	0x93
 #define SIG3	0x0a
-#define PAGE_SIZE	0x20U	//32 words
 
 #elif defined __AVR_ATmega168__
 #define SIG2	0x94
 #define SIG3	0x06
-#define PAGE_SIZE	0x40U	//64 words
 
 #elif defined __AVR_ATmega168P__
 #define SIG2	0x94
 #define SIG3	0x0b
-#define PAGE_SIZE	0x40U	//64 words
 
 #elif defined __AVR_ATmega328P__
 #define SIG2	0x95
 #define SIG3	0x0F
-#define PAGE_SIZE	0x40U	//64 words
 
 #elif defined __AVR_ATmega162__
 #define SIG2	0x94
 #define SIG3	0x04
-#define PAGE_SIZE	0x40U	//64 words
 
 #elif defined __AVR_ATmega163__
 #define SIG2	0x94
 #define SIG3	0x02
-#define PAGE_SIZE	0x40U	//64 words
 
 #elif defined __AVR_ATmega169__
 #define SIG2	0x94
 #define SIG3	0x05
-#define PAGE_SIZE	0x40U	//64 words
 
 #elif defined __AVR_ATmega8515__
 #define SIG2	0x93
 #define SIG3	0x06
-#define PAGE_SIZE	0x20U	//32 words
 
 #elif defined __AVR_ATmega8535__
 #define SIG2	0x93
 #define SIG3	0x08
-#define PAGE_SIZE	0x20U	//32 words
 
 #endif
 
@@ -858,7 +842,7 @@ int noreturn main(void)
 					   [buff] "+y" (bufptr),
 					   [addr] "+z" (address.word),
 					   [length] "+w" (length.word)
-					 : [PGSZ] "M" (PAGE_SIZE),
+					 : [PGSZ] "M" (SPM_PAGESIZE / 2),
 					   [creg] "i" (SPM_CREG_ADDR),
 					   [spmen] "i" (SPM_ENABLE_BIT)
 					 : "r0"
