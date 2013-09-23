212148ddce25ca9b99e4aa56644717bcd4108faa
David P Hilton <david.hilton.p@gmail.com>
2013-09-22 19:50:38 -0600

    library: hexbright/includes moved to hexbright, renamed to pc_stubs, read_adc

--- libraries/hexbright/hexbright.cpp
+++ libraries/hexbright/hexbright.cpp
@@ -32,9 +32,9 @@ either expressed or implied, of the FreeBSD Project.
 #include <limits.h>
 
 #ifndef __AVR // we're not compiling for arduino (probably testing), use these stubs
-#include "includes/NotArduino.h"
+#include "pc_stubs.h"
 #else
-#include "includes/pin_interface.h"
+#include "read_adc.h"
 #include "../digitalWriteFast/digitalWriteFast.h"
 #endif
 
