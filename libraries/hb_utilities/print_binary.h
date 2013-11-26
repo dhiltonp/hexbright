6e363b300fa87a63beef2b724cf4970effec90d4
David Hilton <david.hilton.p@gmail.com>
2013-11-25 23:29:43 -0700

    hb_utilities/print_binary.h: commented out dependency on Serial.h and String.h

--- libraries/hb_utilities/print_binary.h
+++ libraries/hb_utilities/print_binary.h
@@ -2,8 +2,16 @@
 #define PRINT_BINARY_H
 
 #include <hexbright.h>
+/*
+// these 2 includes are needed for print_binary to actually print.
+// Arduino is trying to pull in this file when it's not asked for, causing 
+//  dependency issues/build errors. 
+// Commenting out these includes will mean that any code that calls print_binary
+//  without including Serial.h and String.h in the .ino file will just fail to
+//  print (instead of printing a warning as designed).
 #include <Serial.h>
 #include <String.h>
+*/
 
 // convenience functions that will print 1 or 2 byte values through the serial port.
 extern void print_binary(byte value);
